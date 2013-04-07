d392e085383ad61de40bb561137b8ac99455ec20
Talia Selitsky <talqsel@gmail.com>
2013-04-07 13:54:30 -0400

    first commit

---
+++ BinaryTree2/src/BinaryTree.java
@@ -0,0 +1,45 @@
+
+public class BinaryTree {
+	
+	Node root;
+	
+	public BinaryTree()
+	{
+		root=null;
+	}
+	
+	
+	public class Node
+	{
+		Node left;
+		Node right;
+		int data;
+		
+		public Node(int data)
+		{
+			this.data=data;
+		}
+		
+	}
+	
+	public boolean isBinaryTree()
+	{
+		return isBinaryTree(root);
+	}
+	
+	private boolean isBinaryTree(Node node)
+	{
+		if(node==null) return false;
+		if(node.left.data<node.right.data){
+			isBinaryTree(node.left);
+			isBinaryTree(node.right);
+		}
+		else{
+			return false;
+		}
+		return true;
+	}
+	
+	
+
+}
